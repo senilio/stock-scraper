98c5ff87bffe30f891fdda224d670199d3a58b54
Emil Petersson <git@senil.io>
2021-04-11 13:22:45 +0200

    Slightly modified interval

--- looper.sh
+++ looper.sh
@@ -3,7 +3,7 @@
 while true; do
 	echo "`date`: Running check"
 	python scraper.py
-	sleeptime=$(shuf -i 600-1200 -n 1)
+	sleeptime=$(shuf -i 500-1000 -n 1)
 	echo "Will sleep for $sleeptime seconds..."
 	sleep $sleeptime
 done
